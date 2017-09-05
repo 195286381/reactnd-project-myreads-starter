79c3983f67f6649817e79b33e6ada55f15edb731
朱智伟10191772 <zhu.zhiwei@zte.com.cn>
2017-09-05 10:54:15 +0800

    style: formatter

--- src/component/Bookshelf.js
+++ src/component/Bookshelf.js
@@ -7,11 +7,11 @@ const Bookshelf = ({books, bookshelfTitle}) => (
     <div className="bookshelf-books">
       <ol className="books-grid">
         { 
-	        books.map((book) => (
+          books.map((book) => (
             <li key={Math.random()}>
               <Book book={book} />
             </li>
-	        ))
+          ))
         }
       </ol>
     </div>
